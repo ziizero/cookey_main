dbb1d71138515799dd1396e40e0b613a70ee2d9f
jiyeong <jyfullsun42@gmail.com>
2023-11-23 03:51:01 +0900

    수정

---
+++ app/src/main/java/kr/ac/duksung/cookey_main/MainActivity.java
@@ -0,0 +1,27 @@
+package kr.ac.duksung.cookey_main;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomAppBar bottomAppBar;
+    private FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
